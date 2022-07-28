6d279f34d15f06779935f1b705464c38f80e5ac0
Blue-Silence <blue_silence@outlook.com>
2022-07-28 16:41:40 +0800

    Remaining only case structure type check.

--- TypeCheck.hs
+++ TypeCheck.hs
@@ -33,7 +33,7 @@ checkDecl t (FunDecl tag ts b) = let    funType'=getType t tag
 
 ----------------------------------------------------------------------------------------------------------------------------
 
-tagToType t = SingleType t emptyTypeENV 
+tagToType t = SingleType t emptyTypeENV
 tagToENV x env = (ENV [] [] []):env
 
 checkExpr :: [ENV]->EXPR_C ->Either Type ErrM
@@ -42,51 +42,72 @@ checkExpr es (ConstExpr v mty) = let    ty=case v of
                                             ConstInt _->tagToType [1,3]
                                             ConstChar _->tagToType [1,4]
                                             ConstBool _->tagToType [1,5]
-                                    in case mty of 
+                                    in case mty of
                                             Nothing->Left ty
                                             Just ty'->if ty'==ty then Left ty else Right ("Can't match type. Expected: "++(show ty')++"  Actual: "++(show ty)++"\n")
 checkExpr envs (OpExpr (OP o) es mty t) = case (getType envs o) of
                                             Left ft->let t=funappType (tagToENV t envs) ft es
-                                                            in matchMaybeType mty t 
+                                                            in matchMaybeType mty t
                                             a->a
 checkExpr envs (VarExpr t mty) = matchMaybeType mty (getType envs t)
 checkExpr envs (BlockExpr b mty) = matchMaybeType mty (checkBlock envs [] b)
-checkExpr envs (AppExpr (FunApp fe es) mty tag) = let   envs'=tagToENV tag envs 
-                                                        in case checkExpr envs' fe of 
-                                                                (Right x)->Right x 
+checkExpr envs (AppExpr (FunApp fe es) mty tag) = let   envs'=tagToENV tag envs
+                                                        in case checkExpr envs' fe of
+                                                                (Right x)->Right x
                                                                 (Left ft)->funappType envs' ft es
 checkExpr _ (TypeExpr _) = Left $ tagToType [1,1]
-checkExpr envs (LambdaExpr (Lambda sym b@(Block _ _ tag)) mty) = case mty of 
+checkExpr envs (LambdaExpr (Lambda sym b@(Block _ _ tag)) mty) = case mty of
                                                 Nothing->Right "Error:Lambda need type. \n"
                                                 Just ft->let    (paramTypes,reT)=getFunType tag ft (fmap snd sym)
-                                                                    in case matchType (Left reT) (checkBlock envs paramTypes b) of 
+                                                                    in case matchType (Left reT) (checkBlock envs paramTypes b) of
                                                                             Right x->Right x
-                                                                            Left _->Left ft 
+                                                                            Left _->Left ft
 checkExpr envs a@(LambdaExpr (Lambda sym (BlockTerm (Term e))) mty) = Right ("Illegal lambda: "++show a++"\n")
-checkExpr envs (ConExpr con mty tag) = let  envs'=tagToENV tag envs 
+checkExpr envs (ConExpr con mty tag) = let  envs'=tagToENV tag envs
                                                 in matchMaybeType mty $ checkConS envs' con
-                                                    
 
 
 
 
 funappType :: [ENV]->Type->[EXPR_C]->Either Type ErrM
-funappType envs ft es = let es'=concatEither $ map (checkExpr envs) es 
-                                in case es' of 
-                                    Right x->Right x 
+funappType envs ft es = let es'=concatEither $ map (checkExpr envs) es
+                                in case es' of
+                                    Right x->Right x
                                     Left es''->let  st=foldr (\x acc->ArrowType emptyTypeENV x acc) UnknownRe es''
-                                                    getReType [] a=Left a 
-                                                    getReType (_:xs) (ArrowType _ _ t)=getReType xs t 
+                                                    getReType [] a=Left a
+                                                    getReType (_:xs) (ArrowType _ _ t)=getReType xs t
                                                     getReType _ _=Right "Param number too many \n"
-                                                        in case matchType (Left st) (Left ft) of 
+                                                        in case matchType (Left st) (Left ft) of
                                                             Left t->getReType es t
                                                             a->a
 
 checkConS :: [ENV]->ConStruct->Either Type ErrM
-checkConS = undefined 
+checkConS envs (ConstrIf eb e1 e2 mty)
+    |isBool envs eb = matchMaybeType mty ty
+    |otherwise = Right ("Expect Bool expr in if-structure \n") 
+        where   ty=getGeneralType envs [[e1,e2],[e2,e1]]
+checkConS envs (ConstrWhile eb b mty)
+    |isBool envs eb = matchMaybeType mty $ checkBlock envs [] b 
+    |otherwise = Right ("Expect Bool expr in while-structure \n") 
+checkConS envs (ConReturn e mty) = matchMaybeType mty $ checkExpr envs e
+checkConS envs (ConstrCase ecase pes mty) = undefined 
+
+
+
+getGeneralType envs lt = let    f a (Left b)=Left b
+                                f a _=a 
+                                    in foldl1 f $ map ((foldl1 matchType) . (map (checkExpr envs))) lt
+
+
+
+
+
+isBool env e = case matchType (Left (SingleType [1,5] emptyTypeENV)) (checkExpr env e) of 
+                    Left _->True 
+                    _->False 
 
 matchMaybeType (Just t1) t2 = matchType (Left t1) t2
-matchMaybeType Nothing t2 = t2 
+matchMaybeType Nothing t2 = t2
 
 
 -----------------------------------------------------------------------------------------------------------------------------
@@ -100,50 +121,53 @@ matchType x y = case getRe [x,y] of
                                     failMsg=Right ("Type mismatch. Expect:  "++(show st)++"  Got:  "++(show tt)++"\n")
                                         in case matchTypeH [(0,[],[])] st tt of
                                                 Right _->failMsg
-                                                Left (returnT,as)->case returnT of
-                                                                        Nothing->Left st
-                                                                        Just t->case (concatType t as) of 
-                                                                                    Just t'->Left $ setReturnType st t'
-                                                                                    Nothing->failMsg
-                                                                        
-                                                                         
+                                                Left (unknownT,as)->case sequence $ map (`concatType` as) unknownT of
+                                                                            Just uTs->Left $ setUnknownType uTs st
+                                                                            Nothing->failMsg       
 
-matchTypeH :: [(TagC,[(Tag,Type)],[(Tag,Type)])]->Type->Type->Either (Maybe Type,[(TagC,[(Tag,Type)],[(Tag,Type)])]) ()
+
+
+
+matchTypeH :: [(TagC,[(Tag,Type)],[(Tag,Type)])]->Type->Type->Either ([Type],[(TagC,[(Tag,Type)],[(Tag,Type)])]) ()
 matchTypeH _ (SingleType _ _) (ArrowType _ _ _) = Right ()
-matchTypeH ((tc,stv,ttv):as) t1@(ArrowType (TypeENV _ tye1) t11 t12) t2@(ArrowType (TypeENV _ tye2) t21 t22) 
+matchTypeH as UnknownRe tt = Left ([tt],as)
+matchTypeH ((tc,stv,ttv):as) t1@(ArrowType (TypeENV _ tye1) t11 t12) t2@(ArrowType (TypeENV _ tye2) t21 t22)
     = let   (tc',stv')=addPH tc stv tye1
             (tc'',ttv')=addPH tc' ttv tye2
                 in case matchTypeH ((tc''+1,[],[]):(tc'',stv',ttv'):as) t11 t21 of
-                            Left (_,(a:as))->case t12 of 
-                                                    UnknownRe->Left (Just t22,as) --Provide return value type calculation
-                                                    _->matchTypeH as t12 t22
+                            Left (tts,(a:as))->addLt tts $matchTypeH as t12 t22
                             Right _->Right ()
 matchTypeH a@((tc,stv,ttv):as) t1@(SingleType tag1 (TypeENV _ tye1)) t2@(SingleType tag2 (TypeENV _ tye2))
-    |isTemp tag1 && isTemp tag2 = addN $ setTmpVarType a' tag2 (getTmpVarTypeL a' tag1)
+    |isTemp tag1 && isTemp tag2 = addELt $ setTmpVarType a' tag2 (getTmpVarTypeL a' tag1)
     |isTemp tag1 = Right ()
-    |isTemp tag2 = addN $ setTmpVarType a' tag2 (getTmpVarTypeL a' tag1)
-    |otherwise = if tag1 == tag2 then Left (Nothing,a) else Right ()
+    |isTemp tag2 = addELt $ setTmpVarType a' tag2 (getTmpVarTypeL a' tag1)
+    |otherwise = if tag1 == tag2 then Left ([],a) else Right ()
         where   (tc',stv')=addPH tc stv tye1
                 (tc'',ttv')=addPH tc' ttv tye2
                 a'=(tc'',stv',ttv'):as
 
 matchTypeH ((tc,stv,ttv):as) t1 t2@(SingleType tag2 (TypeENV _ tyenv)) = let    (tc',ttv')=addPH tc ttv tyenv
                                                                                 a'=((tc',stv,ttv'):as)
-                                                                                    in addN $ setTmpVarType a' tag2 t1
+                                                                                    in addELt $ setTmpVarType a' tag2 t1
 matchTypeH ((tc,stv,ttv):as) t1@(ExprType e1 (TypeENV _ tye1)) t2@(ExprType e2 (TypeENV _ tye2)) = let  (tc',stv')=addPH tc stv tye1
                                                                                                         (tc'',ttv')=addPH tc' ttv tye2
                                                                                                         a'=((tc''+1,[],[]):(tc'',stv',ttv'):as)
-                                                                                                                in case matchTypeExpr a' e1 e2 of 
-                                                                                                                        Left (_:as)->addN $ Left as 
+                                                                                                                in case matchTypeExpr a' e1 e2 of
+                                                                                                                        Left (_:as)->addELt $ Left as
                                                                                                                         _->Right()
 matchTypeH a b c = Right () --error ("matchTypeH function not matched. Param: "++(show a)++(show b)++(show c))
 
-addN x = case x of 
+addLt x (Left (xs,ys)) = Left (x++xs,ys)
+addLt _ a = a
+addELt (Left x) = (Left ([],x))
+addELt (Right x) = Right x
+
+addN x = case x of
             Left n->Left (Nothing,n)
             Right n->Right n
-delN x = case x of 
-            Left (_,x)->Left x 
-            Right x->Right x  
+delN x = case x of
+            Left (_,x)->Left x
+            Right x->Right x
 
 addPH tc ttv [] = (tc,ttv)
 addPH tc ttv ((_,t):ts) = addPH (tc+1) ((t,TypePlaceHolder [tc]):ttv) ts
@@ -153,10 +177,10 @@ isTemp x =  case x of
                 _->False
 
 setTmpVarType :: [(TagC,[(Tag,Type)],[(Tag,Type)])]->Tag->Type->Either [(TagC,[(Tag,Type)],[(Tag,Type)])] ()
-setTmpVarType a tag tarTy = let ty=getTmpVarTypeR a tag in 
-                                case ty of 
+setTmpVarType a tag tarTy = let ty=getTmpVarTypeR a tag in
+                                case ty of
                                     (TypePlaceHolder _)->Left $ map (\(a,b,c)->(a,b,map (\(t,ty')->(t,replaceTy ty tarTy ty')) c)) a
-                                    _->delN $ matchTypeH a ty tarTy  
+                                    _->delN $ matchTypeH a ty tarTy
 
 getTmpVarTypeL :: [(TagC,[(Tag,Type)],[(Tag,Type)])]->Tag->Type
 getTmpVarTypeL x = getTmpVarTypeH (concat (map (\(_,a,_)->a) x))
@@ -168,7 +192,7 @@ matchTypeExpr :: [(TagC,[(Tag,Type)],[(Tag,Type)])]->EXPR_C->EXPR_C->Either [(Ta
 matchTypeExpr a (AppExpr (FunApp f1 es1) _ _) (AppExpr (FunApp f2 es2) _ _)
     |length es1 /= length es2 = Right ()
     |otherwise = foldl matchTypeEH (Left a) ((f1,f2):(zip es1 es2))
-        where matchTypeEH a (x,y) = case a of 
+        where matchTypeEH a (x,y) = case a of
                                     (Left a')->matchTypeExpr a' x y
                                     _->Right ()
 matchTypeExpr a (ConstExpr v1 _) (ConstExpr v2 _) = if v1==v2 then Left a else Right ()
@@ -177,25 +201,26 @@ matchTypeExpr a (TypeExpr t1) (TypeExpr t2) = delN $ matchTypeH a t1 t2
 matchTypeExpr _ _ _ = Right()
 
 
-replaceTy ty tarTy t 
+replaceTy ty tarTy t
     |t==ty = tarTy
     |otherwise = t
 
-setReturnType (SingleType _ _) t = t
-setReturnType (ArrowType e t1 t2) t = ArrowType e t1 (setReturnType t2 t)
+setUnknownType (t:_) UnknownRe = t 
+setUnknownType (t:ts) (ArrowType env UnknownRe t2) = ArrowType env t $ setUnknownType ts t2 
+setUnknownType ts (ArrowType env t1 t2) = ArrowType env t1 $ setUnknownType ts t2
 
 
 
-concatType t@(SingleType tag e) as = if isTemp tag 
-                                        then 
-                                            case getTmpVarTypeR' as tag of 
+concatType t@(SingleType tag e) as = if isTemp tag
+                                        then
+                                            case getTmpVarTypeR' as tag of
                                                                 Just (TypePlaceHolder _)->Nothing
                                                                 Just a->Just a
                                                                 Nothing->Just t
                                             else Just t
-concatType (ArrowType e t1 t2) as = do 
-                                        t1'<-concatType t1 as 
-                                        t2'<-concatType t2 as 
+concatType (ArrowType e t1 t2) as = do
+                                        t1'<-concatType t1 as
+                                        t2'<-concatType t2 as
                                         return $ ArrowType e t1' t2'
 concatType (ExprType e env) as = do
                                     e'<-(concatTypeExpr e as)
@@ -204,13 +229,13 @@ concatType _ _ =Nothing
 
 concatTypeExpr t@(ConstExpr _ _) _ = Just t
 concatTypeExpr (TypeExpr e) as = fmap TypeExpr $ concatType e as
-concatTypeExpr t@(VarExpr tag mty) as = if isTemp tag 
-                                        then 
-                                            case getTmpVarTypeR as tag of 
+concatTypeExpr t@(VarExpr tag mty) as = if isTemp tag
+                                        then
+                                            case getTmpVarTypeR as tag of
                                                                 TypePlaceHolder _->Just t
                                                                 a->Just $ TypeExpr a
                                             else Just t
-concatTypeExpr t@(AppExpr (FunApp e es) mty tag) as = do 
+concatTypeExpr t@(AppExpr (FunApp e es) mty tag) as = do
                                                         e'<-concatTypeExpr e as
                                                         es'<-sequence $ fmap (\x->concatTypeExpr x as) es
                                                         return $ AppExpr (FunApp e' es') mty tag
@@ -287,10 +312,10 @@ getRe ((Right x):xs) = case getRe xs of
                         (Left _)->Right x
                         (Right xs')->Right (x++xs')
 
-concatEither t = let    f x acc =case acc of 
+concatEither t = let    f x acc =case acc of
                                 Left as->case x of
                                             Left a->Left (a:as)
-                                            Right x->Right x 
+                                            Right x->Right x
                                 Right e->case x of
                                             Left _->Right e
                                             Right x->Right (x++e)
